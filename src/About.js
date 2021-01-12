88b03143b33a3dbaa09146e7511678b539c0d03e
Q <Q@Skylars-MacBook-Pro.local>
2021-01-11 19:10:59 -0600

    about pg styling

--- src/About.js
+++ src/About.js
@@ -8,66 +8,69 @@ const About = () => (
   <div>
     <div className="pic"></div>
     <p>
-      My name is Quinton Lythgoe and I'm a coder, digital marketer, and animal
-      trainer all wraped in one. Located in sunny Los Angeles, California where
+      My name is Quinton Lythgoe and I'm a full stack developer, digital marketer, and animal
+      trainer all wraped into a big bundle of me.(3 for 1, its a good deal).Located in sunny Los Angeles, California where
       dreams are said to come true and hopefully i can in some way help you
-      accomplish yours On here you will see examples of my work for those
-      interested and working together, and also some of my growth in web
-      development for those interseted in my story.
+      accomplish yours. On here you will see examples of my work for those
+      interested in possibly working together, or if you just wanted to observe some of my growth in web
+      development.
     </p>
     <span>
       <h1>Skills:</h1>
     </span>
     <h2>Coding:</h2>
-    <ul>
       <h3>Front-end</h3>
-      <li className="tech" id="html">
+      <div className='tech-holder'>
+      <p className="tech" id="html">
         HTML
-      </li>
-      <li className="tech" id="css">
+      </p>
+      <p className="tech" id="css">
         CSS
-      </li>
-      <li className="tech" id="sass">
+      </p>
+      <p className="tech" id="sass">
         Javascript
-      </li>
-      <li className="tech" id="react">
+      </p>
+      <p className="tech" id="react">
         React
-      </li>
-    </ul>
-    <ul>
+      </p>
+    </div>
+    <div className='tech-holder'>
       <h3 id="backend">Back-end</h3>
-      <li className="tech" id="express">
+      <p className="tech" id="express">
         Express
-      </li>
-      <li className="tech" id="mongo">
+      </p>
+      <p className="tech" id="mongo">
         MongoDB
-      </li>
-      <li className="tech" id="ruby">
+      </p>
+      <p className="tech" id="ruby">
         Ruby on Rails
-      </li>
-      <li className="tech" id="sql">
+      </p>
+      <p className="tech" id="sql">
         SQL
-      </li>
-    </ul>
+      </p>
+    </div>
     <h1>Experience</h1>
     <div id="project-holder">
       <div className="project-details">
         <img id="pauseapp" src={pauseapp} alt="Pause app" />
-        <p>In this Application me and 3 fellow developers <a href='https://github.com/samuel-casey'>Samuel Casey</a>, <a href='https://github.com/jb0nd87'>Jaime Bond</a>, and <a href='https://github.com/kimrass14'>Kim Rass</a> created "Pause.App" for those who need to take a pause on life and want some help staying on track to execute they're goals. On this one I specifically worked on the back end and constructed the About page</p>
-        <a href=''>See the Code </a>
-        <a href=''>See the live demo </a>
+        <p>In this Application me and 3 fellow developers <a href='https://github.com/samuel-casey'>Samuel Casey</a>, <a href='https://github.com/jb0nd87'>Jaime Bond</a>, and <a href='https://github.com/kimrass14'>Kim Rass</a> created "Pause.App" for those who need to take a pause on life and want some help staying on track to execute they're goals. On this one I helped construct  the back end in which inlcuded creating the necessary models and view controllers. Also constructed the About page</p>
+        <a href='https://github.com/samuel-casey/p3-backend'>See the Code </a>
+     
+        <a id='demo' href='https://pause-app.netlify.app/'>See the live demo </a>
       </div>
       <div className="project-details">
         <img id="real-estate" src={realestate} alt="Real-estate-app" />
         <p>This is an app I created to help users searching for available real estate. Users will be able to view different areas, select how many results they want </p>
-        <a href=''>See the Code </a>
-        <a href=''>See the live demo </a>
+        <a href='https://git.generalassemb.ly/theletterq/project-2-react'>See the Code </a>
+        <br/>
+        {/* <a href=''>See the live demo </a> */}
       </div>
       <div className="project-details">
         <img id="kitchen-pic" src={kitchenpic} alt="Real-estate-app" />
         <p>This is an example in which i re-created that helps users that want to learn how to cook. In my version i added responsiveness, made it mobile first, added the abilities for users to either Log in, Sign Up, or Log out. Also created a Dashboard for once a user is signed in</p>
-        <a href=''>See the Code </a>
-        <a href=''>See the live demo </a>
+        <a href='https://github.com/TheletterQ1/cooking-front'>See the Code </a>
+        <br/>
+        {/* <a href=''>See the live demo </a> */}
       </div>
     </div>
     {/* <h2>Animal Care/Training:</h2>
